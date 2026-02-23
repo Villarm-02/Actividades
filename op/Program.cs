fd5228a96f9c144dea1155b27db516e7235a5391
Villarm-02 <dv2555782@gmail.com>
2026-02-23 11:33:50 -0600

    se hizo el ciclo for.

--- op/Program.cs
+++ op/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int x = 0;
+for (int i = 1; i <=15; i++ )
+{
+    Console.WriteLine(i);
+    x = x + i;
+}
+Console.WriteLine($"\nLa suma es: {x}");
